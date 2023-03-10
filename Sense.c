224c5cd03cf9c1efc7e5ec838b19abcec0e98a0b
hari_08 <97215411+hari06021999@users.noreply.github.com>
2023-03-10 10:26:42 +0530

    Update Sense.c

--- Sense.c
+++ Sense.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdint.h>
 
-struct temp
+struct temp  // Temperature sensor details
 {
-            int slave_add;
-            int data:16;
+            int slave_add; //Tempeature sensor slave address
+            int data:16; // data length
 };
 
-void sense(struct temp sensor);
-struct temp sensor;
+void sense(struct temp sensor); // Sense function Prototype
+struct temp sensor; // Structure Variable or Object
 
 int main()
 {
-            sensor.slave_add=0x54;
-            sensor.data=76;
-            sense(sensor);
+            sensor.slave_add=0x54; // Slave address for Temperature sensor
+            sensor.data=76;  // Temperature sensor data
+            sense(sensor); // Sense function Prototype
             return 0;
 }
 
 void sense(struct temp sensor)
 {
             printf("Data Reading From Temperature Sensor\n");
-            printf(" Slave Address: %x \n", sensor.slave_add);
-            printf(" Sensor data: %d \n", sensor.data);
+            printf(" Slave Address: %x \n", sensor.slave_add);  //Display Temperature sensor Slave addresss
+            printf(" Sensor data: %d \n", sensor.data);  //Display Temperature Sensor Data
 }
